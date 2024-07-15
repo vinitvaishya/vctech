7fc8e11e194cd160fcca559bf2ebb35a4289c0f2
Vinit <vinitvaishya200@gmail.com>
2024-07-15 15:33:25 +0530

    initial commit

--- deviceInfoProject/deviceInfoProject/settings.py
+++ deviceInfoProject/deviceInfoProject/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
